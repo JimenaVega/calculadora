197658efe9142e3234afc5d09729ce4658cfb4d4
AlejoSev <alejoseverini@gmail.com>
2021-04-10 20:26:03 -0300

    Substraction asm code added

--- src/addition.asm
+++ src/addition.asm
@@ -1,15 +1,21 @@
 segment .text
         global  addition
-        global substraction
+        global  substraction
 
 addition:
         enter   0,0              ; make room for sum on stack
 
         mov     eax, [ebp+12]    ; eax = B
-        add     eax, [ebp+8]     ; ecx = ecx + A
+        add     eax, [ebp+8]     ; eax = eax + A
 
         leave
         ret
 
+substraction:
+        enter   0,0             ; make room for sub on stack
 
+        mov     eax, [ebp+8]   ; eax = A
+        sub     eax, [ebp+12]  ; eax = eax - B
 
+        leave
+        ret
