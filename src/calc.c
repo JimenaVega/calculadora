199252d5c78e7276a61c9597057edc1b7594ed68
JimenaVega <jimevega28@gmail.com>
2021-04-10 19:32:19 -0300

    tp1 con errores en linkeo para fixear

---
+++ src/calc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "cdecl.h"
+
+//int PRE_CDECL addition( int, int ) POST_CDECL;
+//int PRE_CDECL substraction( int, int ) POST_CDECL;
+//int addition( int, int ) __attribute__((cdecl));
+
+
+int main(void){
+    int base,n,sum,op,res;
+
+    printf("CALCULADORA\n");
+    printf("Ingrese base de numeración:\n[1] binario [2] decimal\n");
+    scanf("%d", &base);
+
+    printf("Ingrese operacion:\n[1] suma [2] resta\n");
+    scanf("%d", &op);
+
+    printf("Cantidad de numeros a ingresar: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i=0; i<n; i++){
+        printf("Ingrese el numero: ");
+        scanf("%d", &arr[i]);
+    }
+    
+    res = addition(n);
+    /*for (int i=0; i<(n-1); i++){
+        if (i==0){
+            res = addition(arr[i], arr[i+1]);
+        }
+        else{
+            res = addition(res, arr[i+1]);
+        }
+    }*/
+ 
+    printf("Sum is %d\n", res);
+    return 0;
+}
