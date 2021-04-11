3ab2e419db66a32056632b0dc16bc50f28548c4c
JimenaVega <jimevega28@gmail.com>
2021-04-10 21:18:23 -0300

    Input validation added

--- src/addition.asm
+++ src/addition.asm
@@ -14,8 +14,8 @@ addition:
 substraction:
         enter   0,0             ; make room for sub on stack
 
-        mov     eax, [ebp+8]   ; eax = A
-        sub     eax, [ebp+12]  ; eax = eax - B
+        mov     eax, [ebp+8]    ; eax = A
+        sub     eax, [ebp+12]   ; eax = eax - B
 
         leave
         ret
